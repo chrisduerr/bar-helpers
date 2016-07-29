38761ab7b2293bd81c98915bd88b72eabf87b5c2
UndeadLeech <programming@undeadleech.de>
2016-07-29 17:08:52 +0200

    Added Volume Slider.

---
+++ volume_slider/src/main.rs
@@ -0,0 +1,92 @@
+extern crate gtk;
+extern crate regex;
+
+use gtk::prelude::*;
+use gtk::{Window, WindowType, Box, Scale, Adjustment, Orientation};
+use regex::Regex;
+use std::env;
+use std::process::Command;
+
+fn get_current_volume() -> f64 {
+    let output = Command::new("amixer")
+                         .args(&["-D", "pulse", "get", "Master"])
+                         .output();
+    match output {
+        Ok(out) => {
+            let stdout_str = String::from_utf8_lossy(&out.stdout);
+            let re = Regex::new("\\[([0-9]+)%\\]").unwrap();
+            match re.captures(&stdout_str) {
+                Some(caps) => caps.at(1).unwrap().parse().unwrap(),
+                None => 0.0,
+            }
+        },
+        Err(_) => 0.0,
+    }
+}
+
+fn set_volume(level: f64) {
+    let vol_trunc = level as u8;
+    let vol_perc = format!("{}%", vol_trunc);
+    Command::new("amixer")
+            .args(&["-q", "-D", "pulse", "set", "Master", &vol_perc[..]])
+            .spawn().unwrap();
+}
+
+// Check if Scale already is running
+fn is_running() -> bool {
+    let output = Command::new("ps")
+                         .args(&["-ax"])
+                         .output().unwrap();
+    let out_str = String::from_utf8_lossy(&output.stdout);
+    let re = Regex::new("volume_slider").unwrap();
+    let nbr_running = re.find_iter(&out_str).count();
+    (nbr_running >= 2)
+}
+
+fn gotta_kill_em_all() {
+    Command::new("killall").arg("volume_slider").spawn().unwrap();
+}
+
+// Create a new scale
+// If one is already running -> KILL IT
+fn main() {
+    if is_running() {
+        gotta_kill_em_all();
+        return;
+    }
+
+    // Check if bar was specified
+    let args: Vec<_> = env::args().collect();
+    if args.len() <= 1 {
+        return;
+    }
+    let wm_name = format!("volume_slider-{}", args[1]);
+
+    // Init GTK and Window
+    gtk::init().unwrap();
+    let window = Window::new(WindowType::Toplevel);
+    window.set_title(&wm_name[..]);
+    window.set_default_size(350, 50);
+
+    // Create Scale
+    let adj = Adjustment::new(get_current_volume(), 0.0, 101.0, 1.0, 1.0, 1.0);
+    let scale = Scale::new(Orientation::Horizontal, Some(&adj));
+    scale.set_draw_value(false);
+
+    // Create Container
+    let cont = Box::new(Orientation::Horizontal, 0);
+    cont.pack_start(&scale, true, true, 10);
+    window.add(&cont);
+    window.show_all();
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+
+    scale.connect_value_changed(move |scale| {
+        set_volume(scale.get_value());
+    });
+
+    gtk::main();
+}
